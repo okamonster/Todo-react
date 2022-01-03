5398adceba682aa9d59d8ee6388cdb2296db1ce5
okamonster <89242934+okamonster@users.noreply.github.com>
2022-01-03 15:04:14 +0900

    タスク追加実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,28 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
+  const [completeTodos, setCompleteTodos] = useState(["ううう"]);
+
+  const onChangeTodoText = (e) => setTodoText(e.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
 
   return (
     <>
       <div className="input-area">
-        <input placeholder="Todoを追加" />
-        <button>追加</button>
+        <input
+          placeholder="Todoを追加"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTodo</p>
@@ -27,10 +42,14 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のTodo</p>
         <ul>
-          <div className="list-row">
-            <li>unkonow</li>
-            <button>戻す</button>
-          </div>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
